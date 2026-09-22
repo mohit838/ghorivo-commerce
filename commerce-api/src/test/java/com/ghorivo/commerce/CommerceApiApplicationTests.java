9906ac4ac349aca30b81d841c86a5f23a01f8106
Mohit <mohitul449@gmail.com>
2026-09-22 22:48:41 +0600

    feat: initiate new project

---
+++ commerce-api/src/test/java/com/ghorivo/commerce/CommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghorivo.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
